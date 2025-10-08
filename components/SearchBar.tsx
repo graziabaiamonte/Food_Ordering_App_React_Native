6a62546229e6baa399c9c299d9622e66ae5e484e
graziabaiamonte <graziabaiamonte19@gmail.com>
2025-10-08 16:11:26 +0200

    add comments

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -1,46 +1,53 @@
-import {images} from "@/constants";
+import { images } from "@/constants";
 import { router, useLocalSearchParams } from "expo-router";
 import React, { useState } from "react";
 import { Image, TextInput, TouchableOpacity, View } from "react-native";
+import { useDebouncedCallback } from "use-debounce";
 
 const Searchbar = () => {
-    const params = useLocalSearchParams<{ query: string }>();
-    const [query, setQuery] = useState(params.query);
+  const params = useLocalSearchParams<{ query: string }>();
+  const [query, setQuery] = useState(params.query);
 
-    const handleSearch = (text: string) => {
-        setQuery(text);
+  const debounceSearch = useDebouncedCallback(
+    (text: string) => router.push(`/search?query=${text}`),
+    500
+  );
 
-        if(!text) router.setParams({ query: undefined });
-    };
+  const handleSearch = (text: string) => {
+    setQuery(text);
+    debounceSearch(text);
 
-    const handleSubmit = () => {
-        if(query.trim()) router.setParams({ query });
-    }
+    if (!text) router.setParams({ query: undefined });
+  };
 
-    return (
-        <View className="searchbar">
-            <TextInput
-                className="flex-1 p-5"
-                placeholder="Search for pizzas, burgers..."
-                value={query}
-                onChangeText={handleSearch}
-                onSubmitEditing={handleSubmit}
-                placeholderTextColor="#A0A0A0"
-                returnKeyType="search"
-            />
-            <TouchableOpacity
-                className="pr-5"
-                onPress={() => router.setParams({ query })}
-            >
-                <Image
-                    source={images.search}
-                    className="size-6"
-                    resizeMode="contain"
-                    tintColor="#5D5F6D"
-                />
-            </TouchableOpacity>
-        </View>
-    );
+  const handleSubmit = () => {
+    if (query.trim()) router.setParams({ query });
+  };
+
+  return (
+    <View className="searchbar">
+      <TextInput
+        className="flex-1 p-5"
+        placeholder="Search for pizzas, burgers..."
+        value={query}
+        onChangeText={handleSearch}
+        onSubmitEditing={handleSubmit}
+        placeholderTextColor="#A0A0A0"
+        returnKeyType="search"
+      />
+      <TouchableOpacity
+        className="pr-5"
+        onPress={() => router.setParams({ query })}
+      >
+        <Image
+          source={images.search}
+          className="size-6"
+          resizeMode="contain"
+          tintColor="#5D5F6D"
+        />
+      </TouchableOpacity>
+    </View>
+  );
 };
 
 export default Searchbar;
